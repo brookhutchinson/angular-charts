6c1ca1b1866ef546b6ca27fbd4a8e020ee31983c
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-20 08:48:35 -0600

    Create BarChartComponent Register BarChartComponent with AppModule Import BarChartComponent to AppModule Add BarChartComponent to AppModule @NgModule Decorator "declarations" property array Define BarChartComponent TypeScript Define BarChartComponent HTML Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 // modules
-import { NgModule }         from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule }    from '@angular/platform-browser';
-import { ChartsModule }     from 'ng2-charts';
-import { FormsModule }      from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule }          from '@angular/core';
+import { AppRoutingModule }  from './app-routing.module';
+import { BrowserModule }     from '@angular/platform-browser';
+import { ChartsModule }      from 'ng2-charts';
+import { FormsModule }       from '@angular/forms';
+import { HttpClientModule }  from '@angular/common/http';
 
 // components
-import { AppComponent }     from './app.component';
-import { NavbarComponent }  from './components/navbar/navbar.component';
+import { AppComponent }      from './app.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { NavbarComponent }   from './components/navbar/navbar.component';
 
 @NgModule({
   // modules
   imports: [ AppRoutingModule, BrowserModule, ChartsModule, FormsModule, HttpClientModule ],
   // components
-  declarations: [ AppComponent, NavbarComponent ],
+  declarations: [ AppComponent, BarChartComponent, NavbarComponent ],
   // services
   providers: [],
   // bootstrap
