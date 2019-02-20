02b9bb41538b1d602ba93743c0ae819e9a2edab9
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-20 09:22:20 -0600

    Create DoughnutChartComponent Register DoughnutChartComponent with AppModule Import DoughnutChartComponent into AppModule Add DoughnutChartComponent to AppModule @NgModule Decorator "declarations" property array Define DoughnutChartComponent TypeScript Define DoughnutChartComponent HTML Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 // modules
-import { NgModule }          from '@angular/core';
-import { AppRoutingModule }  from './app-routing.module';
-import { BrowserModule }     from '@angular/platform-browser';
-import { ChartsModule }      from 'ng2-charts';
-import { FormsModule }       from '@angular/forms';
-import { HttpClientModule }  from '@angular/common/http';
+import { NgModule }               from '@angular/core';
+import { AppRoutingModule }       from './app-routing.module';
+import { BrowserModule }          from '@angular/platform-browser';
+import { ChartsModule }           from 'ng2-charts';
+import { FormsModule }            from '@angular/forms';
+import { HttpClientModule }       from '@angular/common/http';
 
 // components
-import { AppComponent }      from './app.component';
-import { BarChartComponent } from './components/bar-chart/bar-chart.component';
-import { NavbarComponent }   from './components/navbar/navbar.component';
-import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { AppComponent }           from './app.component';
+import { BarChartComponent }      from './components/bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { NavbarComponent }        from './components/navbar/navbar.component';
+import { PieChartComponent }      from './components/pie-chart/pie-chart.component';
 
 @NgModule({
   // modules
   imports: [ AppRoutingModule, BrowserModule, ChartsModule, FormsModule, HttpClientModule ],
   // components
-  declarations: [ AppComponent, BarChartComponent, NavbarComponent, PieChartComponent ],
+  declarations: [ AppComponent, BarChartComponent, DoughnutChartComponent, NavbarComponent, PieChartComponent ],
   // services
   providers: [],
   // bootstrap
---
+++ src/app/components/doughnut-chart/doughnut-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'doughnut-chart',
+  templateUrl: './doughnut-chart.component.html',
+  styleUrls: [ './doughnut-chart.component.css' ]
+})
+export class DoughnutChartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
