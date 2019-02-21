5421dc8c733bb135a3faefad90d0f29737b92ca9
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-21 09:11:30 -0600

    Setup and configure Angular Router Create Router Config File named named app-routing.module.ts inside src/app folder Define BarChartComponent Route Define DoughnutChartComponent Route Define PieChartComponent Route Define RadarChartComponent Route Export AppRoutingModule from Router Config File Register AppRoutingModule with AppModule Import AppRoutingModule into AppModule Add AppRoutingModule to AppModule @NgModule Decorator "imports" property array Replace "href" html attribute with "routerLink" directive attribute for each menu item link inside NavbarComponent HTML Template Set "Bar Chart" menu item link "routerLink" directive to route value "/bar-chart" Set "Doughnut Chart" menu item link "routerLink" directive to route value "/doughnut-chart" Set "Pie Chart" menu item link "routerLink" directive to route value "/pie-chart" Set "Radar Chart" menu item link "routerLink" directive to route value "/radar-chart" Add "routerLinkActive" directive attribute to each menu item Set "routerLinkActive" directive attribute for each menu item link to a value of "active"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,26 @@
 // modules
-import { NgModule }             from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }               from '@angular/core';
+import { RouterModule, Routes }   from '@angular/router';
+
+// components
+import { BarChartComponent }      from './components/bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { PieChartComponent }      from './components/pie-chart/pie-chart.component';
+import { RadarChartComponent }    from './components/radar-chart/radar-chart.component';
 
 // define routes
-const routes: Routes = [];
+const routes: Routes = [
+  // bar chart route
+  { path: 'bar-chart', component: BarChartComponent },
+  // doughnut chart route
+  { path: 'doughnut-chart', component: DoughnutChartComponent },
+  // pie chart route
+  { path: 'pie-chart', component: PieChartComponent },
+  // radar chart route
+  { path: 'radar-chart', component: RadarChartComponent },
+  // default route
+  { path: '**', component: BarChartComponent }
+];
 
 @NgModule({
   // imports
