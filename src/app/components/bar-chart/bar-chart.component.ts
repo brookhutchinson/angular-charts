506efcbad10995b3f4948be23488578e4072bed0
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-21 12:59:03 -0600

    Define BarChartComponent TypeScript Define BarChartComponent HTML Template Define BarChartComponent CSS Define properties that will be bound to ng2-charts directive attribues on the <canvas> html element Set properties to values Define <div class="canvas-container"> Define <canvas> html element Define ng2-charts directive attributes on <canvas> html element Define a CSS Class named "canvas-container" that will display a <div> using a block layout

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './bar-chart.component.css' ]
 })
 export class BarChartComponent implements OnInit {
+  // properties
+  barChartType = "bar";
+
+  barChartData = [
+    { data: [ 65, 69, 80, 81, 56, 55, 50 ], label: "Series A" },
+    { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: "Series B" }
+  ]
+
+  barChartLabels = [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ];
+
+  barChartLegend = true;
+
+  barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  }
+
   constructor() {}
 
   ngOnInit() {}
