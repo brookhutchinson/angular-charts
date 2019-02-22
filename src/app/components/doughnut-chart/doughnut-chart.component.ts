e4bff7361cd7afeb6d1c657e216574272c41be71
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-22 11:23:54 -0600

    Define DoughnutChartComponent TypeScript Define DoughnutChartComponent HTML Template Define DoughnutChartComponent CSS Define properties that will be bound to ng2-charts directive attributes on the <canvas> html element Define a CSS Class named "canvas-container" that will display a div> using a block layout

--- src/app/components/doughnut-chart/doughnut-chart.component.ts
+++ src/app/components/doughnut-chart/doughnut-chart.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './doughnut-chart.component.css' ]
 })
 export class DoughnutChartComponent implements OnInit {
+  // properties
+  doughnutChartType = 'doughnut';
+
+  doughnutChartData = [ 120, 150, 180, 90 ];
+
+  doughnutChartLabels = [ 'Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4' ];
+
   constructor() {}
 
   ngOnInit() {}
