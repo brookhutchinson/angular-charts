457fe98004bdb5ef02d60d852114b4eb9db3a586
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-25 12:46:02 -0600

    Define RadarChartComponent TypeScript Define RadarChartComponent HTML Template Define RadarChartComponent CSS Define properties that will be bound to ng2-charts-directive attributes on the <canvas> html element Set properties to values Define <div class="canvas-container"> Define <canvas> html element Define ng2-charts directive attributes on <canvas> html element Define a CSS Class named "canvas-container" that will display a <div> using a block layout

--- src/app/components/radar-chart/radar-chart.component.ts
+++ src/app/components/radar-chart/radar-chart.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './radar-chart.component.css' ]
 })
 export class RadarChartComponent implements OnInit {
+  // properties
+  radarChartType = 'radar';
+
+  radarChartData = [
+    { data: [ 120, 130, 180, 70 ], label: '2017' },
+    { data: [ 90, 150, 200, 451 ], label: '2018' }
+  ];
+
+  radarChartLabels = [ 'Q1',  'Q2', 'Q3', 'Q4' ];
+
   constructor() {}
 
   ngOnInit() {}
