972aa013fe1af7fb1e51d2314cb0ab71fcd74812
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-25 13:05:39 -0600

    Define PieChartComponent TypeScript Define PieChartComponent HTML Template Define PieChartComponent CSS Define properties that will be bound to ng2-charts directive attributes on the <canvas> html element Set properties to values Define <div class="canvas-container"> Define <canvas> html element Define ng2-charts directive attributes on <canvas> html element Define a CSS Class named "canvas-container" that will display a <div> using a block layout

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './pie-chart.component.css' ]
 })
 export class PieChartComponent implements OnInit {
+  // properties
+  pieChartType = 'pie';
+
+  pieChartData = [ 120, 150, 180, 90 ];
+
+  pieChartLabels = [ 'Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4' ];
+
   constructor() {}
 
   ngOnInit() {}
